3d08f397d9a23bf4537d992fe1adf10d98e8a155
chamod-ananda <chamodananda9@gmail.com>
2025-08-04 10:01:55 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/springbootdeployment/SpringbootdeploymentApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdeploymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/springbootdeployment/SpringbootdeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
